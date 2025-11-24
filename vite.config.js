21425c56132dad63093b6494cdda67eae9c48041
MaddyB <43785086+maddyb99@users.noreply.github.com>
2025-11-24 18:18:08 +0530

    fix tailwind dependeny and tsconfig issues

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,14 @@
-// vite.config.js
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
-// https://vitejs.dev/config/
+import path from "path"
+import tailwindcss from "@tailwindcss/vite"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {              
+  plugins: [react(), tailwindcss()],
+  resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-});
+})
